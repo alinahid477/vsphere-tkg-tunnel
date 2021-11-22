a535ca97b142cf5ba3d920407e1095e9e4eb98ec
Ali Nahid <alinahid@gmail.com>
2021-11-23 07:03:59 +1100

    fluent-bit elastic search

---
+++ binaries/tanzuwizard/elasticsearch/dockerhubregcred.sh
@@ -0,0 +1,9 @@
+if [[ -z $1 || -z $2 ]]
+then
+    printf "\nERROR: You must provide/pass dockerhub username and password in the parameter."
+    printf "\n\t parameter 1: username"
+    printf "\n\t parameter 2: password"
+    printf "\n\n"
+    exit 1
+fi
+kubectl create secret docker-registry dockerhubregcred --docker-server=https://index.docker.io/v2/ --docker-username=$1 --docker-password=$2 --docker-email=your@email.com --namespace tanzu-system-logging
